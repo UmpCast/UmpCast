7c871570045f78247d1d8b953972fb0b7e03114c
Jonathan Kao <jkao97@yahoo.com>
2021-07-31 11:26:00 -0700

    feat: add organization model, admin

---
+++ server/core/models/organization.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+from django.utils.timezone import now
+
+
+class Organization(models.Model):
+    name = models.CharField(unique=True, max_length=64)
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="UserOrganization",
+    )
+
+    email = models.EmailField(max_length=64, blank=True, null=True)
+    website_url = models.CharField(max_length=64, blank=True, null=True)
+
+    date_joined = models.DateTimeField(default=now, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class UserOrganization(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    is_owner = models.BooleanField(default=False)
