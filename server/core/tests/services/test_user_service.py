875616c87c1d46446400d94038ef3983358bccd2
Jonathan Kao <jkao97@yahoo.com>
2021-08-06 19:09:42 -0700

    test: add additional user service tests

--- server/core/tests/services/test_user_service.py
+++ server/core/tests/services/test_user_service.py
@@ -1,7 +1,7 @@
 from core.services import UserService
 from django.test import TestCase
 from model_bakery import baker
-from core.models import User
+from core.models import User, Season, Organization, UserSeason
 
 
 class UserServiceTest(TestCase):
@@ -10,3 +10,47 @@ class UserServiceTest(TestCase):
         user_service = UserService(user)
         current_user = user_service.current_user()
         self.assertEqual(user, current_user)
+
+    def test_owned_organizations(self):
+        user = baker.make(User)
+        user_service = UserService(user)
+
+        organization = baker.make(Organization)
+        organization.members.add(user, through_defaults={"is_owner": True})
+
+        self.assertEqual(
+            1,
+            len(user_service.owned_organizations()),
+        )
+
+    def test_admin_seasons(self):
+        user = baker.make(User)
+        user_service = UserService(user)
+
+        baker.make(
+            UserSeason,
+            user=user,
+            season=baker.make(Season),
+            permission_type="admin",
+        )
+
+        self.assertEqual(
+            1,
+            len(user_service.admin_seasons()),
+        )
+
+    def test_referee_seasons(self):
+        user = baker.make(User)
+        user_service = UserService(user)
+
+        baker.make(
+            UserSeason,
+            user=user,
+            season=baker.make(Season),
+            permission_type="referee",
+        )
+
+        self.assertEqual(
+            1,
+            len(user_service.referee_seasons()),
+        )
