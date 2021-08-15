9d9eb3eeb0a2d7fbf330eb43d7f8533bee393597
Jonathan Kao <jkao97@yahoo.com>
2021-08-14 17:35:26 -0700

    feat: add fields to user schema (#17)
    * fix: user object type should have fields attribute
    
    * feat: add methods to user_service
    
    * feat: create organizations, season types, add to user type
    
    * test: add additional user service tests
    
    * feat: add user schema permissions
    
    * feat: refactor user schema fields, user permissions
    
    * test: finish user service tests
    
    * test: add user permission tests

--- server/core/services/users/services.py
+++ server/core/services/users/services.py
@@ -1,4 +1,4 @@
-from core.models import User
+from core.models import User, Organization, Season
 
 
 class UserService(object):
@@ -7,3 +7,37 @@ class UserService(object):
 
     def current_user(self) -> User:
         return self.user
+
+    def owned_organizations(self) -> list[Organization]:
+        return Organization.objects.filter(
+            userorganization__user=self.user,
+            userorganization__is_owner=True,
+        )
+
+    def member_organizations(self) -> list[Organization]:
+        return Organization.objects.filter(
+            userorganization__user=self.user,
+            userorganization__is_owner=False,
+        )
+
+    def all_organizations(self) -> list[Organization]:
+        return Organization.objects.filter(
+            userorganization__user=self.user,
+        )
+
+    def admin_seasons(self) -> list[Season]:
+        return Season.objects.filter(
+            userseason__user=self.user,
+            userseason__permission_type="admin",
+        )
+
+    def referee_seasons(self) -> list[Season]:
+        return Season.objects.filter(
+            userseason__user=self.user,
+            userseason__permission_type="referee",
+        )
+
+    def all_seasons(self) -> list[Season]:
+        return Season.objects.filter(
+            userseason__user=self.user,
+        )
---
+++ server/schema/core/types.py
@@ -0,0 +1,3 @@
+from .users.types import UserType
+from .seasons.types import SeasonType
+from .organizations.types import OrganizationType
