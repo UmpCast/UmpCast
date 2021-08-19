3b1dcdae725ffc0d5b07f7f4ff2135039bfdaad9
Victor Lin <41597231+Cozieee@users.noreply.github.com>
2021-08-18 17:42:30 -0700

    ci: client apollo config (#50)
    * feat(client): initialize running expo client
    
    * chore(client): remove conflicting apollo packages
    
    * feat(client): extract config to expo app.json
    
    * refactor(client): remove unused npm scripts
    
    * refactor(client): organize .gitignore
    
    * ci(client): add apollo extension config and support
    
    * refactor(client): update .gitignore with expo-shared and yarn-error

---
+++ client/App.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import Constants from 'expo-constants';
+import { NativeBaseProvider } from "native-base"
+import { ApolloProvider, ApolloClient } from "@apollo/client"
+
+import {localSchema, clientCache} from "./src/apollo/clientConfiguration"
+import { NavigationContainer } from "@react-navigation/native"
+
+
+function environmentConfig(node_env: string) {
+    switch(node_env) {
+        case "development":
+            return Constants.manifest?.extra?.DEVELOPMENT
+        case "production":
+            return Constants.manifest?.extra?.PRODUCTION
+        default:
+            null
+    }
+}
+
+export function App() {
+
+    const {NODE_ENV} = process.env
+    if (!NODE_ENV) return null
+
+    const config = environmentConfig(NODE_ENV)
+
+    const client = new ApolloClient({
+        uri: config.server_uri,
+        cache: clientCache,
+        typeDefs: localSchema
+    })
+
+    return (
+        <ApolloProvider client={client}>
+            <NativeBaseProvider>
+                <NavigationContainer>
+                </NavigationContainer>
+            </NativeBaseProvider>
+        </ApolloProvider>
+    )
+}
+
+export default App
