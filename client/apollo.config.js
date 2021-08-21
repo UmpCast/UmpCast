8d9884d4b3e13ab92cc4704f7246ca80edda354c
Victor Lin <vvictor.llin@gmail.com>
2021-08-20 22:46:36 -0700

    feat(client): use relative path with apollo config

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -1,8 +1,10 @@
+import path from 'path'
+
 module.exports = {
   client: {
     service: {
       name: 'umpcast-client',
-      localSchemaFile: 'client/src/schema.graphql',
+      localSchemaFile: path.resolve(__dirname, 'src/schema.graphql'),
     },
     includes: ['src/**/*.{ts,tsx}', 'App.tsx'],
     excludes: ['**/__tests__/**', 'src/{generated,mocks}/*'],
