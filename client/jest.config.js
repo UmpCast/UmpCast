4c218b44f68f1e7ad87eea8fd7d96e0397323e86
Victor Lin <vvictor.llin@gmail.com>
2021-08-21 10:41:09 -0700

    fix(client): revise configs post merge

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
-  collectCoverage: true,
   collectCoverageFrom: [
     'src/{app,navigation}/**/*.{ts,tsx}',
   ],
