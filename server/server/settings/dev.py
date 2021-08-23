6f6727ea705a0d6efb605bd760e788d6741423b8
Victor Lin <vvictor.llin@gmail.com>
2021-08-23 02:58:08 -0700

    fix(server): enable server cors

--- server/server/settings/dev.py
+++ server/server/settings/dev.py
@@ -8,3 +8,17 @@ GRAPHQL_JWT = {
     "JWT_EXPIRATION_DELTA": timedelta(seconds=20),
     "JWT_REFRESH_EXPIRATION_DELTA": timedelta(minutes=1),
 }
+
+INSTALLED_APPS = [
+    *INSTALLED_APPS,
+    'corsheaders'
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    *MIDDLEWARE
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:19006'
+]
