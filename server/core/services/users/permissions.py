a4d564843cabd450633566e8b420c1b1f8efe099
Jonathan Kao <jkao97@yahoo.com>
2021-08-23 10:08:23 -0700

    feat: add user mutation create, update permissions

--- server/core/services/users/permissions.py
+++ server/core/services/users/permissions.py
@@ -2,14 +2,22 @@ from core.models import User
 
 
 class UserPermission(object):
-    def __init__(self, user):
+    def __init__(self, user: User):
         self.user = user
 
-    def has_resolve_organizations_permission(self, authenticated_user) -> bool:
+    def has_resolve_organizations_permission(self, authenticated_user: User) -> bool:
         return self.is_user_owner(authenticated_user)
 
-    def has_resolve_seasons_permission(self, authenticated_user) -> bool:
+    def has_resolve_seasons_permission(self, authenticated_user: User) -> bool:
         return self.is_user_owner(authenticated_user)
 
     def is_user_owner(self, authenticated_user: User) -> bool:
         return self.user == authenticated_user
+
+    @staticmethod
+    def has_create_permission(authenticated_user: User) -> bool:
+        return True
+
+    @staticmethod
+    def has_update_permission(user_id: int, authenticated_user: User) -> bool:
+        return user_id == authenticated_user.id
