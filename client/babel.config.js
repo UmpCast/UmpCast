f361f376df78836c298306d930e4660559b7c23f
Victor Lin <vvictor.llin@gmail.com>
2021-08-26 06:13:19 -0700

    fix(client): remove svg transformer in favor of manually converting rsvg

--- client/babel.config.js
+++ client/babel.config.js
@@ -3,7 +3,6 @@ module.exports = (api) => {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            'inline-react-svg',
             [
                 'module-resolver',
                 {
