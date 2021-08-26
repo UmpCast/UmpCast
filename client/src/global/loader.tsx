f25658f42d4ac4e32b22df13c11a37a00bb0810d
Victor Lin <vvictor.llin@gmail.com>
2021-08-26 07:08:07 -0700

    fix(client): prettier checks pass

--- client/src/global/loader.tsx
+++ client/src/global/loader.tsx
@@ -1,7 +1,6 @@
 import Loader from 'app/provider/models/LoaderOptions'
 import { loaderOptionsVar, appLoadingVar } from 'global/reactiveVars'
 
-
 export const startLoading = (loaderOptions: Loader = {}) => {
     loaderOptionsVar(loaderOptions)
     appLoadingVar(true)
