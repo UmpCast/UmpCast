52ed350dd20a6e3fa04c58f7b767eeddf6323c43
Victor Lin <vvictor.llin@gmail.com>
2021-08-30 05:43:05 -0700

    fix(client): useFakeTimers to pass tests

--- client/src/app/provider/__tests__/appProvider.test.tsx
+++ client/src/app/provider/__tests__/appProvider.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import { render, waitFor } from '@testing-library/react-native'
+import {
+    render,
+    waitFor
+} from '@testing-library/react-native'
 import { Text } from 'native-base'
 
 import MockNativeBaseProvider from 'utils/__mocks__/mockNativeBaseProvider'
@@ -10,13 +13,13 @@ import * as utils from '../providerUtils'
 
 describe('AppProvider', () => {
     it('displays a splash until app is initialized, when on Web', async () => {
+        jest.useFakeTimers()
         jest.mock('react-native/Libraries/Utilities/Platform', () => ({
             OS: 'Web'
         }))
 
-        jest.spyOn(utils, 'initializeApp').mockResolvedValue(
-            new Promise((resolve) => setTimeout(resolve, 200))
-        )
+        const fakeInitialize = Promise.resolve()
+        jest.spyOn(utils, 'initializeApp').mockResolvedValue(fakeInitialize)
 
         const { queryByText } = render(
             <AppProvider>
@@ -26,7 +29,8 @@ describe('AppProvider', () => {
         )
 
         await waitFor(() => expect(queryByText('UmpCast')).not.toBeNull())
+        await waitFor(() => fakeInitialize)
         await waitFor(() => expect(queryByText('UmpCast')).toBeNull())
-        await waitFor(() => expect(queryByText('placeholder')).not.toBeNull())
+        expect(queryByText('placeholder')).not.toBeNull()
     })
 })
