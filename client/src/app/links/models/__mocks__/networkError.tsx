3aded6261023a8fc40690dd3fc51636f2ff5c264
Victor Lin <vvictor.llin@gmail.com>
2021-08-30 13:51:50 -0700

    fix(client): missing prettier file write

--- client/src/app/links/models/__mocks__/networkError.tsx
+++ client/src/app/links/models/__mocks__/networkError.tsx
@@ -3,7 +3,6 @@ import faker from 'faker'
 
 import NetworkError from '../networkError'
 
-
 export const MockNetworkError = Factory.Sync.makeFactory<NetworkError>({
     name: 'Network Error',
     message: Factory.each(() => faker.lorem.words(5))
