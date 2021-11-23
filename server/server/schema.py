ef275f07c8b15683c1fa0a8a2bd3eea2ac9b9237
Jonathan Kao <jkao97@yahoo.com>
2021-11-23 12:31:39 -0800

    refactor: remove schema imports

--- server/server/schema.py
+++ server/server/schema.py
@@ -1,14 +1,12 @@
 import graphene
 import graphql_jwt
-from schema.core.query import Query as CoreQuery
-from schema.core.mutation import Mutation as CoreMutation
 
 
-class Query(CoreQuery, graphene.ObjectType):
+class Query(graphene.ObjectType):
     pass
 
 
-class Mutation(CoreMutation, graphene.ObjectType):
+class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
