16b40e4a67b597b7a4d4df1c6572e048f6bad190
Victor Lin <vvictor.llin@gmail.com>
2021-12-12 19:02:33 +0000

    refactor(client) remove files unrelated to branch

--- client/src/app/organization/models/Organizations.tsx
+++
@@ -1,5 +0,0 @@
-import { GetManagerOrgs_me_portals_manager_organization } from '../graphql/queries/__generated__/GetManagerOrgs'
-
-export type Organization = GetManagerOrgs_me_portals_manager_organization
-
-export type Organizations = Organization[]
