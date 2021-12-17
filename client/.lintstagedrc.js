60cd7fd6e361a4bd5a9cc83681e3360ed306d520
Victor Lin <vvictor.llin@gmail.com>
2021-12-17 10:20:14 +0000

    fix: remove src folder checking

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': 'eslint_d --fix --cache ./src',
-    '*.{js,jsx,ts,tsx}': 'prettier --write ./src'
+    '*.{js,jsx,ts,tsx}': 'eslint_d --fix',
+    '*.{js,jsx,ts,tsx}': 'prettier --write'
 }
