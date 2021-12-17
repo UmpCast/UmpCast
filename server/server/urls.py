c549823719f6c7b13ef9858d35fd6ad1d1eeefc1
Jonathan Kao <jkao97@yahoo.com>
2021-12-17 01:51:23 -0500

    feat: init ariadne graphql schema

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ariadne.contrib.django.views import GraphQLView
+from schema.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("graphql/", GraphQLView.as_view(schema=schema)),
 ]
