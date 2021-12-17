d1b40e8bd7ce18934daad2614677b9096c076a56
Victor Lin <vvictor.llin@gmail.com>
2021-12-17 23:36:19 +0000

    feat: add sendEmailVerification mock mutation

--- client/src/app/__generated__/globalTypes.ts
+++ client/src/app/__generated__/globalTypes.ts
@@ -7,6 +7,11 @@
 // START Enums and Input Objects
 //==============================================================
 
+export interface SendEmailVerificationInput {
+    route: string
+    email: string
+}
+
 //==============================================================
 // END Enums and Input Objects
 //==============================================================
---
+++ client/src/app/authentication/graphql/mutations/__generated__/SendEmailVerification.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { SendEmailVerificationInput } from './../../../../__generated__/globalTypes'
+
+// ====================================================
+// GraphQL mutation operation: SendEmailVerification
+// ====================================================
+
+export interface SendEmailVerification {
+    sendEmailVerification: boolean | null
+}
+
+export interface SendEmailVerificationVariables {
+    input: SendEmailVerificationInput
+}
