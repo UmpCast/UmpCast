b423f3866afb7848e2330cece1106ace5f8d799c
Victor Lin <41597231+vlin02@users.noreply.github.com>
2021-12-16 22:57:51 -0800

    build(android): EAS dev/preview/prod for Android (#249)
    * build: add boilerplate eas config
    
    * refactor: rename web deploment action
    
    * build: add expo deployment action
    
    * fix: lower apollo client version for successful metro compilation
    
    * fix: entrypoint does not work with eas
    
    * chore: add service account file to gitignore
    
    * fix: include new App entrypoint
    
    * feat: add production base eas config
    
    * feat: add production release channel
    
    * build: add owner to expo manifest
    
    * build: add expo-dev-client module
    
    * build: add development build config
    
    * build: update slug
    
    * build: set runtimeVersion relative to SDK version
    
    * build: set app name dynamically
    
    * build: use dev-client for dev script
    
    * build: set APP_ENV for all profiles
    
    * refactor: remove unecessary releaseChannel for dev
    
    * build: enable auto-submit on expo_deploy
    
    * fix: switch package name by APP_ENV
    
    * fix: use underscores for android package name
    
    * build: add preview build
    
    * fix: include breaks into switch statement
    
    * build: add reusable setup action for client workflows
    
    * fix: convert action file to directory
    
    * fix: convert expo token into action input
    
    * fix: set working directory for steps
    
    * build: add caching to expo & node_modules installation
    
    * build: make expo installation optional
    
    * refactor: remove unused setup workflow
    
    * build: set all client workflows to use setup action
    
    * refactor: rename client workflows and jobs appropriately
    
    * build: use setup action for web deploy
    
    * refactor: rename build action to submit
    
    * fix: restrict paths for publish & submit actions
    
    * refactor: remove inconsistent quotations in actions
    
    * refactor: rename workflow job to main
    
    * fix: correct expo publish command
    
    * chore: testing client_expo_publish action
    
    * fix: set default working directory
    
    * build: optimize assets
    
    * chore: testing client_expo_submit action
    
    * fix: include playstore service account
    
    * fix: revert client_expo_submit action testing

--- client/app.config.js
+++ client/app.config.js
@@ -1,15 +1,30 @@
-import 'dotenv/config'
+let build = {}
+
+switch (process.env.APP_ENV) {
+    case 'development':
+        build.name = 'UmpCast (DEV)'
+        build.androidPackage = 'com.umpcast.umpcast_dev'
+        break
+    case 'preview':
+        build.name = 'UmpCast (TEST)'
+        build.androidPackage = 'com.umpcast.umpcast_test'
+        break
+    case 'production':
+    default:
+        build.name = 'UmpCast'
+        build.androidPackage = 'com.umpcast.umpcast'
+        break
+}
 
 export default {
     expo: {
-        name: 'UmpCast',
-        slug: 'umpcast',
+        name: build.name,
+        slug: 'UmpCast',
+        owner: 'umpcast',
         version: '1.0.0',
+        sdkVersion: '43.0.0',
+        runtimeVersion: '43.0',
         orientation: 'portrait',
-        entryPoint:
-            process.env.NODE_ENV === 'development'
-                ? 'src/AppDev.tsx'
-                : 'src/App.tsx',
         icon: './assets/icon.png',
         splash: {
             image: './assets/splash.png',
@@ -17,13 +32,15 @@ export default {
             backgroundColor: '#ffffff'
         },
         updates: {
-            fallbackToCacheTimeout: 0
+            fallbackToCacheTimeout: 30
         },
         assetBundlePatterns: ['**/*'],
         ios: {
             supportsTablet: true
         },
         android: {
+            package: build.androidPackage,
+            versionCode: 5,
             adaptiveIcon: {
                 foregroundImage: './assets/adaptive-icon.png',
                 backgroundColor: '#FFFFFF'
