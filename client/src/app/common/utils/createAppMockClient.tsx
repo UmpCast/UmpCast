6750837b7fc42c8a213e53121093eab4263e087a
Victor Lin <vvictor.llin@gmail.com>
2021-12-19 05:09:02 +0000

    refactor: rename mock client to avoid collision

--- client/src/app/common/utils/createAppMockClient.tsx
+++ client/src/app/common/utils/createAppMockClient.tsx
@@ -1,10 +1,12 @@
 import { HttpLink, ApolloClient } from '@apollo/client'
+import fetch from 'isomorphic-fetch'
 import AppCache from '@/apollo/appCache'
+import { loadAppExtra } from './appBuild'
 
-export default function createMockClient() {
+export default function createAppMockClient() {
     const httpLink = new HttpLink({
         fetch,
-        uri: 'http://localhost:8000/graphql'
+        uri: loadAppExtra().SERVER_GRAPHQL_URL
     })
 
     return new ApolloClient({
