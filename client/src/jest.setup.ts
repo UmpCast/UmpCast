aa620a0835e993eeb3ed4aa0a9ca92eb33f6d137
Victor Lin <vvictor.llin@gmail.com>
2021-12-19 06:29:52 +0000

    fix: ignore wip error caused by rntl

--- client/src/jest.setup.ts
+++ client/src/jest.setup.ts
@@ -1,7 +1,22 @@
-import { drop } from '@mswjs/data'
-import faker from 'faker'
 import mswDB from './msw/mswDB'
 import { mswServer } from './msw/mswServer'
+import { drop } from '@mswjs/data'
+import faker from 'faker'
+
+const originalError = console.error.bind(console.error)
+
+beforeEach(() =>
+    jest
+        .spyOn(console, 'error')
+        .mockImplementation(
+            (msg) =>
+                !msg
+                    .toString()
+                    .includes(
+                        'Warning: You called act(async () => ...) without await.'
+                    ) && originalError(msg)
+        )
+)
 
 jest.mock('@react-native-async-storage/async-storage', () => ({
     __esModule: true,
