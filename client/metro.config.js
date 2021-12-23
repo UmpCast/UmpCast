d11dd6a82cbd7ace7fac9e4ea096418e9fc5d813
Victor Lin <vvictor.llin@gmail.com>
2021-12-23 04:14:38 +0000

    fix: support latest apollo client with metro patch

---
+++ client/metro.config.js
@@ -0,0 +1,28 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const defaultSourceExts =
+    require('metro-config/src/defaults/defaults').sourceExts
+module.exports = {
+    transformer: {
+        getTransformOptions: () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true
+            }
+        })
+    },
+    resolver: {
+        sourceExts: process.env.RN_SRC_EXT
+            ? [
+                  ...process.env.RN_SRC_EXT.split(',').concat(
+                      defaultSourceExts
+                  ),
+                  'cjs'
+              ] // <-- cjs added here
+            : [...defaultSourceExts, 'cjs'] // <-- cjs added here
+    }
+}
