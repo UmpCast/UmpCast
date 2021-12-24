7c9cd9e6143fac720c4dc5cf8c79b516ae2482e0
Jonathan Kao <jkao97@yahoo.com>
2021-12-23 16:58:02 -0800

    feat(server): firebase auth service (#264)
    * feat: init auth service
    
    * feat: add firebase_admin dep
    
    * feat: add auth service objects, service class
    
    * feat: add firebase creds
    
    * feat: implement get context value firebase
    
    * ci: add secrets to gh actions
    
    * test: add auth user dataclass tests
    
    * test: replace isInstance calls with assertIsInstance method
    
    * test: add auth service tests
    
    * refactor: remove unneeded var
    
    * test: add auth context tests

---
+++ server/auth/services/service.py
@@ -0,0 +1,15 @@
+from auth.services import AuthUser, FireBaseUser, AnonymousUser
+
+
+class AuthService:
+    @classmethod
+    def get_firebase_user(cls, id: str, email: str, email_verified: bool) -> AuthUser:
+        return FireBaseUser(
+            id=id,
+            email=email,
+            email_verified=email_verified,
+        )
+
+    @classmethod
+    def get_anonymous_user(cls) -> AuthUser:
+        return AnonymousUser()
