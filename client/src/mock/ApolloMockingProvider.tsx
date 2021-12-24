cbfec39fcebfda09b8a10298ec38ca3dcb1d81c1
Victor Lin <vvictor.llin@gmail.com>
2021-12-24 01:42:25 +0000

    feat: add ApolloMockingProvider

---
+++ client/src/mock/ApolloMockingProvider.tsx
@@ -0,0 +1,31 @@
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { addMocksToSchema, IMocks } from '@graphql-tools/mock'
+import { ApolloClient, ApolloProvider } from '@apollo/client'
+import { SchemaLink } from '@apollo/client/link/schema'
+import React from 'react'
+import typeDefs from './typeDefs'
+import AppCache from '@/app/common/utils/appCache'
+
+export interface ApolloMockingProviderProps {
+    mocks?: IMocks | undefined
+    children: JSX.Element
+}
+
+export default function ApolloMockingProvider({
+    mocks = undefined,
+    children
+}: ApolloMockingProviderProps) {
+    const schema = makeExecutableSchema({ typeDefs })
+
+    const schemaWithMocks = addMocksToSchema({
+        schema,
+        mocks
+    })
+
+    const client = new ApolloClient({
+        link: new SchemaLink({ schema: schemaWithMocks }),
+        cache: AppCache
+    })
+
+    return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
