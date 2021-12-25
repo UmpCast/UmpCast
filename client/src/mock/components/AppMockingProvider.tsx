7769910ab029d2e929e41489637ae90c18b5d548
Victor Lin <vvictor.llin@gmail.com>
2021-12-25 02:36:57 +0000

    fix: use mock native base provider

--- client/src/mock/components/AppMockingProvider.tsx
+++ client/src/mock/components/AppMockingProvider.tsx
@@ -5,7 +5,26 @@ import ApolloMockingProvider, {
     ApolloMockingProviderProps
 } from './ApolloMockingProvider'
 
-export type AppMockingProviderProps = ApolloMockingProviderProps
+export interface AppMockingProviderProps extends ApolloMockingProviderProps {}
+
+export interface MockNativeBaseProviderProps {
+    children: JSX.Element
+}
+
+export function MockNativeBaseProvider({
+    children
+}: MockNativeBaseProviderProps) {
+    return (
+        <NativeBaseProvider
+            initialWindowMetrics={{
+                frame: { x: 0, y: 0, width: 0, height: 0 },
+                insets: { top: 0, left: 0, right: 0, bottom: 0 }
+            }}
+        >
+            {children}
+        </NativeBaseProvider>
+    )
+}
 
 export default function AppMockingProvider({
     children,
@@ -14,9 +33,9 @@ export default function AppMockingProvider({
 }: AppMockingProviderProps) {
     return (
         <ApolloMockingProvider mocks={mocks} logging={logging}>
-            <NativeBaseProvider>
+            <MockNativeBaseProvider>
                 <AppNavigationContainer>{children}</AppNavigationContainer>
-            </NativeBaseProvider>
+            </MockNativeBaseProvider>
         </ApolloMockingProvider>
     )
 }
