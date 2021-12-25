83a4cc752506b672968275501db4bfec05ceee8a
Victor Lin <vvictor.llin@gmail.com>
2021-12-25 02:07:19 +0000

    fix: ignore async storage warning

---
+++ client/src/mock/scenarios/authScenarios.ts
@@ -0,0 +1,27 @@
+import { loadAppExtra } from '@/app/common/utils/appBuild'
+import { sendSignInLinkToEmail, getAuth } from 'firebase/auth'
+
+const authScenarios = {
+    EMAIL_VERIF_DEFAULT: {
+        Mutation: () => ({
+            sendEmailVerification: (obj: any) => {
+                const redirectUrl = new URL(obj.route, loadAppExtra().APP_URL)
+
+                sendSignInLinkToEmail(getAuth(), obj.input.email, {
+                    url: redirectUrl.href,
+                    android: {
+                        packageName: loadAppExtra().ANDROID_PACKAGE
+                    },
+                    handleCodeInApp: true,
+                    dynamicLinkDomain: loadAppExtra().DYNAMIC_LINK_DOMAIN
+                })
+
+                return {
+                    errors: null
+                }
+            }
+        })
+    }
+}
+
+export default authScenarios
