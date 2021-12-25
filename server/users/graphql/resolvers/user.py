4b185c7242a4248abcf29144eb981e7302548fd3
Jonathan Kao <jkao97@yahoo.com>
2021-12-24 21:18:54 -0800

    feat: implement user type resolvers

--- server/users/graphql/resolvers/user.py
+++ server/users/graphql/resolvers/user.py
@@ -1,17 +1,19 @@
 from users.graphql.types import user
 from datetime import datetime
+from users.services import User
+from ariadne.types import GraphQLResolveInfo
 
 
 @user.field("id")
-def resolve_id(obj, info) -> int:
-    pass
+def resolve_id(obj: User, info: GraphQLResolveInfo) -> str:
+    return obj.id
 
 
 @user.field("email")
-def resolve_email(obj, info) -> int:
-    pass
+def resolve_email(obj: User, info: GraphQLResolveInfo) -> int:
+    return obj.email
 
 
 @user.field("date_created")
-def resolve_date_created(obj, info) -> datetime:
-    pass
+def resolve_date_created(obj: User, info: GraphQLResolveInfo) -> datetime:
+    return obj.date_created
