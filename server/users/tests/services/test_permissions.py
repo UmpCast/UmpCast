62eee9d514737cecb2f980345439ce7673d14106
Jonathan Kao <jkao97@yahoo.com>
2021-12-24 21:36:32 -0800

    test: add user permission tests

---
+++ server/users/tests/services/test_permissions.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from users.services import UserPermission
+from auth.services import AuthUser
+
+
+class TestUserPermission(TestCase):
+    def test_has_resolve_me_permission(self):
+        auth_user: AuthUser = AuthUser(
+            id="1234567890",
+            email="ben_franklin@upenn.edu",
+            email_verified=True,
+        )
+        user_permission: UserPermission = UserPermission(auth_user)
+
+        has_permission: bool = user_permission.has_resolve_me_permission()
+
+        self.assertTrue(has_permission)
