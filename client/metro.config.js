8097cf1e7e747fb617c306b2e196639e377c9915
Victor Lin <vvictor.llin@gmail.com>
2021-12-25 21:13:04 +0000

    refactor: simplify metro config

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,9 +1,3 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
 const defaultSourceExts =
     require('metro-config/src/defaults/defaults').sourceExts
 module.exports = {
@@ -16,13 +10,6 @@ module.exports = {
         })
     },
     resolver: {
-        sourceExts: process.env.RN_SRC_EXT
-            ? [
-                  ...process.env.RN_SRC_EXT.split(',').concat(
-                      defaultSourceExts
-                  ),
-                  'cjs'
-              ] // <-- cjs added here
-            : [...defaultSourceExts, 'cjs'] // <-- cjs added here
+        sourceExts: [...defaultSourceExts, 'cjs']
     }
 }
