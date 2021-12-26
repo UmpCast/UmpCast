ba944d85baa97b7ea5ff6be29486651c257112f0
Jonathan Kao <jkao97@yahoo.com>
2021-12-26 00:18:33 -0800

    fix: rename lambda arg

--- server/schema/types.py
+++ server/schema/types.py
@@ -2,4 +2,6 @@ from ariadne import QueryType, ScalarType
 
 query = QueryType()
 
-datetime_scalar = ScalarType("DateTime", serializer=lambda x: x.isoformat())
+datetime_scalar = ScalarType(
+    "DateTime", serializer=lambda date_time: date_time.isoformat()
+)
