78e96ed4d9f92f98f8602165b4ef699ee1cf4670
Jonathan Kao <jkao97@yahoo.com>
2021-12-26 00:06:29 -0800

    fix: server allows 0.0.0.0 host

--- server/server/settings/base.py
+++ server/server/settings/base.py
@@ -26,7 +26,9 @@ SECRET_KEY = "django-insecure-secret-key"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+]
 
 # Application definition
 
