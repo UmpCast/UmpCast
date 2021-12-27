b3836dbedbacb45470aa336049a8a51fb14bc050
Jonathan Kao <jkao97@yahoo.com>
2021-12-26 18:13:43 -0800

    feat: update schema.py

--- server/schema/schema.py
+++ server/schema/schema.py
@@ -1,5 +1,7 @@
 from ariadne import make_executable_schema, load_schema_from_path
-from schema.types import query, datetime_scalar
+from ariadne.contrib.django.scalars import date_scalar, datetime_scalar
+
+from schema.types import query, mutation
 from schema.resolvers import *
 from users.graphql.types import user
 from users.graphql.resolvers import *
@@ -9,6 +11,7 @@ type_defs = load_schema_from_path(".")  # loads all .graphql files
 schema = make_executable_schema(
     type_defs,
     query,
-    datetime_scalar,
+    mutation, 
     user,
+    [date_scalar, datetime_scalar]
 )
