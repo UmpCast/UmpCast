ac21de3b2db00e389c19f7a70bf2ec30361f4718
Jonathan Kao <jkao97@yahoo.com>
2021-12-28 15:24:43 -0800

    feat: add typing to utils/secrets

--- server/utils/secrets.py
+++ server/utils/secrets.py
@@ -3,7 +3,7 @@ import base64
 import json
 
 
-def get_secret(client, secret_name, secret_key) -> str:
+def get_secret(client, secret_name: str, secret_key: str) -> str:  # type: ignore
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
@@ -32,11 +32,14 @@ def get_secret(client, secret_name, secret_key) -> str:
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
         if "SecretString" in get_secret_value_response:
             secret = get_secret_value_response["SecretString"]
-            json_secret = json.loads(secret)
+            json_secret: dict[str, str] = json.loads(secret)
             return json_secret.get(secret_key, "")
         else:
             decoded_binary_secret = base64.b64decode(
                 get_secret_value_response["SecretBinary"]
             )
-            json_decoded_binary_secret = json.loads(decoded_binary_secret)
+            json_decoded_binary_secret: dict[str, str] = json.loads(
+                decoded_binary_secret
+            )
             return json_decoded_binary_secret.get(secret_key, "")
+    return ""
