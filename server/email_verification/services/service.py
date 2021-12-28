3bbcf6ce6b81f56421bb075f7951d51beeb63969
Jonathan Kao <jkao97@yahoo.com>
2021-12-28 01:21:40 -0800

    feat: add typing to email_verification services

--- server/email_verification/services/service.py
+++ server/email_verification/services/service.py
@@ -1,6 +1,6 @@
 from .email_verification_input import EmailVerificationInput
 from pydantic import ValidationError
-from firebase_admin import auth
+from firebase_admin import auth  # type: ignore
 from django.core.mail import send_mail
 
 
@@ -10,11 +10,11 @@ class EmailVerificationService:
 
     def get_email_verification_link(
         self,
-        url,
-        ios_bundle_id,
-        android_package_name,
-        android_minimum_version,
-        dynamic_link_domain,
+        url: str,
+        ios_bundle_id: str,
+        android_package_name: str,
+        android_minimum_version: str,
+        dynamic_link_domain: str,
     ) -> str:
         action_code_settings = auth.ActionCodeSettings(
             url=url,
@@ -26,13 +26,13 @@ class EmailVerificationService:
             dynamic_link_domain=dynamic_link_domain,
         )
         email = self.email_verification_input.email
-        link = auth.generate_email_verification_link(
+        link: str = auth.generate_email_verification_link(
             email=email,
             action_code_settings=action_code_settings,
         )
         return link
 
-    def send_email_verification_email(self, link) -> None:
+    def send_email_verification_email(self, link: str) -> None:
         email = self.email_verification_input.email
         send_mail(
             subject="UmpireCast Email Verification",
