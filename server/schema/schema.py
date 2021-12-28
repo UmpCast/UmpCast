79720ed6b428e0203f7f9d58e0f6a9811f69542d
Jonathan Kao <jkao97@yahoo.com>
2021-12-27 17:24:02 -0800

    feat: implement InputError schema type

--- server/schema/schema.py
+++ server/schema/schema.py
@@ -1,8 +1,9 @@
 from ariadne import make_executable_schema, load_schema_from_path
 from ariadne.contrib.django.scalars import date_scalar, datetime_scalar
 
-from schema.types import query, mutation
+from schema.types import query, mutation, input_error
 from schema.resolvers import *
+
 from users.graphql.types import user
 from users.graphql.resolvers import *
 
@@ -11,7 +12,8 @@ type_defs = load_schema_from_path(".")  # loads all .graphql files
 schema = make_executable_schema(
     type_defs,
     query,
-    mutation, 
+    mutation,
+    input_error,
     user,
-    [date_scalar, datetime_scalar]
+    [date_scalar, datetime_scalar],
 )
