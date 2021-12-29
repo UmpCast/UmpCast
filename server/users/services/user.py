cbceac62d4286c6aa14d658765130370599c5eee
Jonathan Kao <jkao97@yahoo.com>
2021-12-28 18:40:14 -0800

    feat: update user dataclass, tests

--- server/users/services/user.py
+++ server/users/services/user.py
@@ -4,6 +4,15 @@ from datetime import datetime
 
 @dataclass(frozen=True)
 class User:
+    # from User
     id: str
     email: str
     date_created: datetime
+    # from UserProfile
+    first_name: str
+    last_name: str
+    street_address: str
+    city: str
+    state: str
+    zip_code: int
+    phone_number: str
