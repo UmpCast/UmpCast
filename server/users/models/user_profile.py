b84f149b9cd9dc5544520654cef05782e46b7dd2
Jonathan Kao <jkao97@yahoo.com>
2021-12-28 18:37:24 -0800

    feat: add __str__ method to user profile model

--- server/users/models/user_profile.py
+++ server/users/models/user_profile.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     first_name = models.CharField(max_length=64)
@@ -15,3 +14,6 @@ class UserProfile(models.Model):
     zip_code = models.IntegerField()
 
     phone_number = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return f"{self.user} UserProfile"
