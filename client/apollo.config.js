575ba1802fce972f515088fbc2201963a6526647
Victor Lin <vvictor.llin@gmail.com>
2021-12-30 06:57:18 +0000

    chore: remove apollo cli and config

--- client/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    client: {
-        service: {
-            name: 'umpcast-client',
-            localSchemaFile: 'src/mock/schema.graphql'
-        },
-        excludes: ['**/__tests__/*']
-    }
-}
