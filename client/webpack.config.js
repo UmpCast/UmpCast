e284b590cd9a55cbf2a00fb2c269edcc3ad8f50a
Victor Lin <vvictor.llin@gmail.com>
2021-12-30 10:10:13 +0000

    fix: add custom webpack config due to mjs incompatability

---
+++ client/webpack.config.js
@@ -0,0 +1,12 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config')
+
+module.exports = async function (env, argv) {
+    const config = await createExpoWebpackConfigAsync(env, argv)
+    config.module.rules.push({
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+        use: []
+    })
+
+    return config
+}
