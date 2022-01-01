72d2b1381349fe4542bf8e50f56a2966ce3f0f37
Victor Lin <vvictor.llin@gmail.com>
2022-01-01 06:12:25 +0000

    chore: run eslint & prettier

--- client/src/mock/utils/schema.ts
+++ client/src/mock/utils/schema.ts
@@ -3,6 +3,7 @@ import base from '../typeDefs/base.graphql'
 import common from '../typeDefs/common.graphql'
 import email from '../typeDefs/email.graphql'
 import user from '../typeDefs/user.graphql'
+
 const mockSchema = makeExecutableSchema({
     typeDefs: [base, common, email, user]
 })
