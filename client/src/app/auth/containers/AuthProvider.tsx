e384c38dddca40418ca2550abf452f3aa8c9ac75
Victor Lin <vvictor.llin@gmail.com>
2022-01-02 01:14:00 +0000

    feat: support all auth states in auth provider

--- client/src/app/auth/containers/AuthProvider.tsx
+++ client/src/app/auth/containers/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { useGetMyInfoLazyQuery, useGetMyInfoQuery } from '@/app/generated-types'
 import { getAuth } from 'firebase/auth'
 import { useEffect, useState } from 'react'
 
@@ -13,12 +14,21 @@ export interface AuthProviderProps {
 
 export default function AuthProvider({ render }: AuthProviderProps) {
     const [authState, setAuthState] = useState<AuthState | null>(null)
+    const [getMyInfo, { data }] = useGetMyInfoLazyQuery()
 
     useEffect(() => {
-        getAuth().onAuthStateChanged((user) => {
+        return getAuth().onAuthStateChanged((user) => {
             if (!user) return setAuthState(AuthState.UNAUTHENTICATED)
+            getMyInfo()
         })
     }, [])
+
+    useEffect(() => {
+        if (!data) return
+        setAuthState(
+            data?.me ? AuthState.AUTHENTICATED : AuthState.UNREGISTERED
+        )
+    }, [data?.me])
 
     if (!authState) return null
 
--- client/src/app/generated-types.ts
+++ client/src/app/generated-types.ts
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client'
 import * as Apollo from '@apollo/client'
-
 export type Maybe<T> = T | null
 export type InputMaybe<T> = Maybe<T>
 export type Exact<T extends { [key: string]: unknown }> = {
@@ -111,6 +110,27 @@ export type UserPayload = {
     user?: Maybe<User>
 }
 
+export type GetMyInfoQueryVariables = Exact<{ [key: string]: never }>
+
+export type GetMyInfoQuery = {
+    __typename?: 'Query'
+    me?:
+        | {
+              __typename?: 'User'
+              id: string
+              firstName: string
+              lastName: string
+              fullAddress: string
+              streetAddress: string
+              city: string
+              state: string
+              zipCode: number
+              phoneNumber: string
+          }
+        | null
+        | undefined
+}
+
 export type SendSignInLinkMutationVariables = Exact<{
     email: Scalars['String']
     actionCodeSettings: ActionCodeSettingsInput
@@ -127,6 +147,69 @@ export type SendSignInLinkMutation = {
     }
 }
 
+export const GetMyInfoDocument = gql`
+    query GetMyInfo {
+        me {
+            id
+            firstName
+            lastName
+            fullAddress
+            streetAddress
+            city
+            state
+            zipCode
+            phoneNumber
+        }
+    }
+`
+
+/**
+ * __useGetMyInfoQuery__
+ *
+ * To run a query within a React component, call `useGetMyInfoQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetMyInfoQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetMyInfoQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetMyInfoQuery(
+    baseOptions?: Apollo.QueryHookOptions<
+        GetMyInfoQuery,
+        GetMyInfoQueryVariables
+    >
+) {
+    const options = { ...defaultOptions, ...baseOptions }
+    return Apollo.useQuery<GetMyInfoQuery, GetMyInfoQueryVariables>(
+        GetMyInfoDocument,
+        options
+    )
+}
+export function useGetMyInfoLazyQuery(
+    baseOptions?: Apollo.LazyQueryHookOptions<
+        GetMyInfoQuery,
+        GetMyInfoQueryVariables
+    >
+) {
+    const options = { ...defaultOptions, ...baseOptions }
+    return Apollo.useLazyQuery<GetMyInfoQuery, GetMyInfoQueryVariables>(
+        GetMyInfoDocument,
+        options
+    )
+}
+export type GetMyInfoQueryHookResult = ReturnType<typeof useGetMyInfoQuery>
+export type GetMyInfoLazyQueryHookResult = ReturnType<
+    typeof useGetMyInfoLazyQuery
+>
+export type GetMyInfoQueryResult = Apollo.QueryResult<
+    GetMyInfoQuery,
+    GetMyInfoQueryVariables
+>
 export const SendSignInLinkDocument = gql`
     mutation SendSignInLink(
         $email: String!
