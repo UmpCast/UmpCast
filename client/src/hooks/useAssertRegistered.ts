fb2287a10a73582837b4ab115c09a3eab8393334
Victor Lin <vvictor.llin@gmail.com>
2022-01-05 07:43:09 +0000

    chore: revert useAssertRegistered

--- client/src/hooks/useAssertRegistered.ts
+++ client/src/hooks/useAssertRegistered.ts
@@ -1,21 +1,17 @@
 import { appVar } from '@/apollo/appCache'
 import { useGetMyIdLazyQuery, AuthState } from '@/apollo/generated'
-import { useEffect } from 'react'
 
 export default function useAssertRegistered() {
-    const [getMyId, { data }] = useGetMyIdLazyQuery()
+    const [getMyId] = useGetMyIdLazyQuery()
 
-    useEffect(() => {
-        if (appVar.authState() !== AuthState.Unauthenticated) return
+    const assertRegistered = async () => {
+        const { data } = await getMyId()
 
         const newAuthState = data?.me
             ? AuthState.Authenticated
             : AuthState.Unregistered
-
         appVar.authState(newAuthState)
-    }, [data])
-
-    const assertRegistered = getMyId
+    }
 
     return assertRegistered
 }
