c00a5cd4b4b3b2623c5e049817e69309bbc1f204
Jonathan Kao <jkao97@yahoo.com>
2022-01-07 15:07:51 -0800

    feat: init graphql server

--- server/users/app.py
+++ server/users/app.py
@@ -1,12 +1,19 @@
 from models import db
 from starlette.applications import Starlette
 import uvicorn
+from ariadne import load_schema_from_path
+from ariadne.asgi import GraphQL
+from ariadne.contrib.federation import make_federated_schema
 
 async def setup_db():
     await db.set_bind("postgresql://postgres:postgres@users-db:5432/postgres")
     await db.gino.create_all()
     
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_federated_schema(type_defs)
+
 app = Starlette(debug=True, on_startup=[setup_db])
+app.mount("/graphql", GraphQL(schema),)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
