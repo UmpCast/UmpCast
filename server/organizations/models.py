4e3ba31275c3b4ca8aa3f2a9ba8bfcae90489373
Jonathan Kao <jkao97@yahoo.com>
2022-01-08 17:34:46 -0800

    feat: implement Season, Division, Position (#332)
    * feat: implement Season, Division, Position
    
    * feat: update schema
    
    * feat: implement Season resolvers
    
    * feat: implement Division resolvers
    
    * feat: implement Position resolvers
    
    * feat: consistent resolver naming
    
    * feat: implement id resolvers

--- server/organizations/models.py
+++ server/organizations/models.py
@@ -10,3 +10,29 @@ class Organization(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String)
     website_url = db.Column(db.String)
+
+
+class Season(db.Model):
+    __tablename__ = "seasons"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    organization_id = db.Column(db.Integer, db.ForeignKey("organizations.id"))
+    name = db.Column(db.String)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+
+class Division(db.Model):
+    __tablename__ = "divisions"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    season_id = db.Column(db.Integer, db.ForeignKey("seasons.id"))
+    name = db.Column(db.String)
+
+
+class Position(db.Model):
+    __tablename__ = "positions"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    division_id = db.Column(db.Integer, db.ForeignKey("divisions.id"))
+    name = db.Column(db.String)
