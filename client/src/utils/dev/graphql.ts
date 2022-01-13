52c2d2a084b203b4d8642b7bd009104135344448
Victor Lin <vvictor.llin@gmail.com>
2022-01-13 01:28:34 +0000

    fix: update graphql path

--- client/src/utils/dev/graphql.ts
+++ client/src/utils/dev/graphql.ts
@@ -1,10 +1,10 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
 // graphql plugin import errors on absolute paths
-import base from '../graphql/typeDefs/base.graphql'
-import common from '../graphql/typeDefs/common.graphql'
-import email from '../graphql/typeDefs/email.graphql'
-import user from '../graphql/typeDefs/user.graphql'
+import base from '../../graphql/typeDefs/base.graphql'
+import common from '../../graphql/typeDefs/common.graphql'
+import email from '../../graphql/typeDefs/email.graphql'
+import user from '../../graphql/typeDefs/user.graphql'
 
 export const mockSchema = makeExecutableSchema({
     typeDefs: [base, common, email, user]
