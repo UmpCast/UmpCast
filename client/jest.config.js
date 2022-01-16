9b1b3e618acb31e052cba405e7743600f563d745
Victor Lin <41597231+vlin02@users.noreply.github.com>
2022-01-15 17:49:31 -0800

    fix: react navigation background cutoff (#369)
    * fix: replace native-stack with stack navigation due to background cutoff
    
    * chore: run eslint & prettier
    
    * fix: lower jest version due to removal of setImmediate mock
    
    * chore: run eslint & prettier

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'jest-expo',
     setupFilesAfterEnv: [
         '@testing-library/jest-native/extend-expect',
+        './node_modules/react-native-gesture-handler/jestSetup.js',
         './src/jest.setup.ts'
     ],
     restoreMocks: true,
