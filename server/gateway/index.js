d3d6649b1a938adcce3b6af52b2578ab4915e8c6
Jonathan Kao <jkao97@yahoo.com>
2022-01-17 12:33:16 -0800

    feat: add permits service to gateway

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -1,15 +1,21 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway } = require('@apollo/gateway');
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
-    serviceList: [
-        { name: 'users', url: 'http://users-service:8000/graphql' },
-        { name: 'organizations', url: 'http://organizations-service:8000/graphql' },
-    ]
+  serviceList: [
+    { name: "users", url: "http://users-service:8000/graphql" },
+    { name: "organizations", url: "http://organizations-service:8000/graphql" },
+    { name: "permits", url: "http://permits-service:8000/graphql" },
+  ],
 });
 
-const server = new ApolloServer({gateway});
+const server = new ApolloServer({ gateway });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Gateway ready at ${url}`);
-}).catch(err => {console.error(err)});
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
