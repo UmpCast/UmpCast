086a9a61f35c522b8ca742fb6b4af65f2292cce4
Victor Lin <vvictor.llin@gmail.com>
2022-01-17 20:56:30 +0000

    feat: add defaults to theme

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,12 +1,29 @@
 import { extendTheme } from 'native-base'
 
 const appTheme = extendTheme({
+    colors: {
+        primary: {
+            '1': '#a5b4fc',
+            '2': '#6366f1',
+            '3': '#3730a3'
+        },
+        secondary: {
+            '1': '#cbd5e1',
+            '2': '#64748b',
+            '3': '#1e293b'
+        }
+    },
     components: {
         Input: {
             baseStyle: {
                 _focus: {
-                    borderColor: 'indigo.500'
+                    borderColor: 'primary.2'
                 }
+            }
+        },
+        Icon: {
+            defaultProps: {
+                size: 5
             }
         }
     }
