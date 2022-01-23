6bc436f1da1581432753fefb01d56fd2a54827ba
Jonathan Kao <jkao97@yahoo.com>
2022-01-23 06:33:06 -0800

    feat: add firebase id to request in gateway (#350)
    * chore: add prettier
    
    * feat: add aws, firebase deps
    
    * feat: implement auth tokens
    
    * feat: convert prettier file to yaml
    
    * refactor: use prettier formatting
    
    * refactor: use named exports
    
    * fix: add eol to yaml

---
+++ server/gateway/firebase_auth.js
@@ -0,0 +1,17 @@
+import admin from 'firebase-admin'
+
+
+const firebaseApp = admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
+})
+
+
+async function verifyIdToken(token) {
+    return await firebaseApp.auth().verifyIdToken(token)
+}
+
+export { verifyIdToken } 
