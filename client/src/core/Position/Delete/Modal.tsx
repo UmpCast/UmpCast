17d5f9b048a7522481f4941aa022e5053031ef25
Victor Lin <vvictor.llin@gmail.com>
2022-01-29 04:11:32 +0000

    feat: add position delete modal

---
+++ client/src/core/Position/Delete/Modal.tsx
@@ -0,0 +1,36 @@
+import { Modal, Button, Text } from 'native-base'
+
+export default function PositionDeleteModal({
+    isOpen,
+    onClose,
+    onConfirm
+}: {
+    isOpen: boolean
+    onClose: () => any
+    onConfirm: () => any
+}) {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} testID="division-delete-modal">
+            <Modal.Content>
+                <Modal.Header>Delete Position</Modal.Header>
+                <Modal.Body>
+                    <Text>Are you sure want to delete this position?</Text>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button.Group space={2}>
+                        <Button
+                            colorScheme="blueGray"
+                            onPress={onClose}
+                            variant="ghost"
+                        >
+                            Cancel
+                        </Button>
+                        <Button colorScheme="danger" onPress={onConfirm}>
+                            Confirm
+                        </Button>
+                    </Button.Group>
+                </Modal.Footer>
+            </Modal.Content>
+        </Modal>
+    )
+}
