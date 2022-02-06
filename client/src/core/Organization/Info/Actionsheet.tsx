697e4a59892f9f4026608229009c04987d74a83d
Victor Lin <vvictor.llin@gmail.com>
2022-02-06 23:10:42 +0000

    feat: add org/info/actionsheet action items

--- client/src/core/Organization/Info/Actionsheet.tsx
+++ client/src/core/Organization/Info/Actionsheet.tsx
@@ -1,16 +1,53 @@
-import { OrganizationInfoActionsheetFragment } from '@/generated'
+import {
+    OrganizationInfoActionsheetFragment,
+    OrganizationPermissionLevel
+} from '@/generated'
+import { AntDesign } from '@expo/vector-icons'
 import {
     Actionsheet,
     Divider,
     Heading,
+    HStack,
+    Icon,
     Image,
     Text,
     useDisclose,
-    VStack
+    VStack,
+    Pressable,
+    IPressableProps,
+    ITextProps,
+    Button
 } from 'native-base'
 
 interface OrganizationInfoActionsheetProps {
     permit: OrganizationInfoActionsheetFragment
+}
+
+export function ActionItem({ children, ...rest }: IPressableProps) {
+    return (
+        <Pressable
+            px={3}
+            py={2}
+            bgColor="blueGray.200"
+            _hover={{ bgColor: 'blueGray.300' }}
+            {...rest}
+        >
+            <HStack space={4} alignItems="center">
+                {children}
+            </HStack>
+        </Pressable>
+    )
+}
+
+export function ActionText(props: ITextProps) {
+    return (
+        <Text
+            fontSize="xs"
+            fontWeight="medium"
+            color="blueGray.600"
+            {...props}
+        />
+    )
 }
 
 export default function OrganizationInfoActionsheet({
@@ -33,7 +70,7 @@ export default function OrganizationInfoActionsheet({
                             borderRadius={5}
                         />
                     )}
-                    <VStack space={1}>
+                    <VStack space={2}>
                         <Heading fontSize="md" color="blueGray.700">
                             {title}
                         </Heading>
@@ -42,6 +79,79 @@ export default function OrganizationInfoActionsheet({
                         </Text>
                     </VStack>
                     <Divider color="blueGray.200" />
+                    <VStack>
+                        <ActionItem borderTopRadius={5}>
+                            <Icon
+                                as={AntDesign}
+                                name="team"
+                                size={4}
+                                color="indigo.500"
+                            />
+                            <ActionText>Members</ActionText>
+                        </ActionItem>
+                        <ActionItem borderBottomRadius={5}>
+                            <Icon
+                                as={AntDesign}
+                                name="bells"
+                                size={4}
+                                color="indigo.500"
+                            />
+                            <ActionText>Notifications</ActionText>
+                        </ActionItem>
+                    </VStack>
+                    {permissionLevel === OrganizationPermissionLevel.Owner ? (
+                        <VStack space={2}>
+                            <Text fontSize="xs" color="blueGray.400">
+                                OWNER ONLY
+                            </Text>
+                            <VStack>
+                                <ActionItem borderTopRadius={5}>
+                                    <Icon
+                                        as={AntDesign}
+                                        name="clockcircleo"
+                                        size={4}
+                                        color="indigo.500"
+                                    />
+                                    <ActionText>Seasons</ActionText>
+                                </ActionItem>
+                                <ActionItem>
+                                    <Icon
+                                        as={AntDesign}
+                                        name="file1"
+                                        size={4}
+                                        color="indigo.500"
+                                    />
+                                    <ActionText>Templates</ActionText>
+                                </ActionItem>
+                                <ActionItem>
+                                    <Icon
+                                        as={AntDesign}
+                                        name="edit"
+                                        size={4}
+                                        color="indigo.500"
+                                    />
+                                    <ActionText>Profile</ActionText>
+                                </ActionItem>
+                                <ActionItem borderBottomRadius={5}>
+                                    <Icon
+                                        as={AntDesign}
+                                        name="wallet"
+                                        size={4}
+                                        color="indigo.500"
+                                    />
+                                    <ActionText>Billing</ActionText>
+                                </ActionItem>
+                            </VStack>
+                        </VStack>
+                    ) : null}
+                    <Button
+                        bgColor="blueGray.200"
+                        _hover={{ bgColor: 'blueGray.300' }}
+                    >
+                        <Text color="indigo.500" fontWeight="medium">
+                            Leave Organization
+                        </Text>
+                    </Button>
                 </VStack>
             </Actionsheet.Content>
         </Actionsheet>
