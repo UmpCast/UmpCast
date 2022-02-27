00cb6eb16983a8abcc724693eea8fb7256729e0e
Victor Lin <vvictor.llin@gmail.com>
2022-02-27 22:51:24 +0000

    test: add org/member/screen test

--- client/src/core/Org/Member/Screen.spec.tsx
+++ client/src/core/Org/Member/Screen.spec.tsx
@@ -5,6 +5,41 @@ class Setup extends BaseSetup {
     constructor() {
         super(<OrgMemberScreen />)
     }
+
+    withMembers() {
+        const {
+            Query: { organization }
+        } = this.resolvers
+
+        organization.mockImplementationOnce(() => {
+            return {
+                id: 'organization-1',
+                memberList: [
+                    {
+                        user: {
+                            firstName: 'User',
+                            lastName: '1'
+                        },
+                        permissionLevel: 'OWNER'
+                    },
+                    {
+                        user: {
+                            firstName: 'User',
+                            lastName: '2'
+                        },
+                        permissionLevel: 'MEMBER'
+                    }
+                ]
+            }
+        })
+    }
 }
 
-it('shows organization members', () => {})
+it('shows organization members', async () => {
+    const setup = new Setup()
+
+    setup.withMembers()
+    const api = setup.render()
+    await api.findByText(/user 1/i)
+    await api.findByText(/user 2/i)
+})
