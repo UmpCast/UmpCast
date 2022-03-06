98dcd83ac9b1f2cebba680fb60d19820f779dee7
Jonathan Kao <jkao97@yahoo.com>
2022-03-05 23:27:41 -0800

    feat: add schema folder

--- server/users_service/users_service/urls.py
+++ server/users_service/users_service/urls.py
@@ -17,9 +17,19 @@ from django.contrib import admin
 from django.urls import path
 
 from ariadne_django.views import GraphQLView
-from schema import schema
+from schema.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(schema=schema), name="graphql"),
+    path(
+        "graphql/",
+        GraphQLView.as_view(
+            schema=schema,
+            context_value=lambda request: {
+                "request": request,
+                "user-id": request.headers.get("user-id", None),
+            },
+        ),
+        name="graphql",
+    ),
 ]
