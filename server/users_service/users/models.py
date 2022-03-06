74ba3c6b30d1adee2845081d3d48ee48449c5b6f
Jonathan Kao <jkao97@yahoo.com>
2022-03-06 00:30:27 -0800

    feat: user model fields nullable

--- server/users_service/users/models.py
+++ server/users_service/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class User(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=255, null=True)
