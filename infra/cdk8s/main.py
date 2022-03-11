4c48d7efcdd4222f34413d34347d50089df0b2d5
Jonathan Kao <jkao97@yahoo.com>
2022-03-10 18:41:27 -0800

    feat: init cdk8s repo (#432)
    * feat: init cdk8s repo
    
    * feat: add black dev deps
    
    * feat: add UmpCastService cdk8s config
    
    * feat: add users UmpCastService
    
    * feat: add isort deps
    
    * feat: add cdk8s gh workflow
    
    * feat: pass cspell.json
    
    * feat: fix isort
    
    * feat: fix isort again
    
    * fix: cspell

---
+++ infra/cdk8s/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from constructs import Construct
+
+from cdk8s import App, Chart
+from service import UmpCastService
+
+
+class MyChart(Chart):
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+
+        UmpCastService(
+            self,
+            id="users",
+            image="jonathankao97/umpcast-users-service:latest",
+        )
+
+
+app = App()
+MyChart(app, "cdk8s")
+
+app.synth()
---
+++ infra/cdk8s/service.py
@@ -0,0 +1,59 @@
+from constructs import Construct, Node
+from imports import k8s
+
+
+class UmpCastService(Construct):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        image: str,
+        replicas=1,
+        port=80,
+        container_port=8080,
+    ):
+        super().__init__(scope, id)
+
+        label = {"app": Node.of(self).id}
+
+        k8s.KubeService(
+            self,
+            "service",
+            spec=k8s.ServiceSpec(
+                type="ClusterIP",
+                ports=[
+                    k8s.ServicePort(
+                        port=port,
+                        target_port=k8s.IntOrString.from_number(
+                            container_port,
+                        ),
+                    ),
+                ],
+                selector=label,
+            ),
+        )
+
+        k8s.KubeDeployment(
+            self,
+            "deployment",
+            spec=k8s.DeploymentSpec(
+                replicas=replicas,
+                selector=k8s.LabelSelector(match_labels=label),
+                template=k8s.PodTemplateSpec(
+                    metadata=k8s.ObjectMeta(labels=label),
+                    spec=k8s.PodSpec(
+                        containers=[
+                            k8s.Container(
+                                name="server",
+                                image=image,
+                                ports=[
+                                    k8s.ContainerPort(
+                                        container_port=container_port,
+                                    ),
+                                ],
+                            )
+                        ]
+                    ),
+                ),
+            ),
+        )
