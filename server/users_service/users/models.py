7a40ed8ce064dd5b1485740ff67ebf715fde94bc
Jonathan Kao <jkao97@yahoo.com>
2022-03-19 12:19:31 -0400

    feat: model fields blank=true

--- server/users_service/users/models.py
+++ server/users_service/users/models.py
@@ -7,11 +7,11 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    street_address = models.CharField(max_length=255, null=True)
-    city = models.CharField(max_length=255, null=True)
-    state = models.CharField(max_length=255, null=True)
-    zip_code = models.CharField(max_length=5, null=True)
-    phone_number = models.CharField(max_length=10, null=True)
+    street_address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    zip_code = models.CharField(max_length=5, null=True, blank=True)
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
