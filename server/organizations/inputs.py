456ccc8d20f9efcca9a60f7271ef35004c1a4e9d
Jonathan Kao <jkao97@yahoo.com>
2022-03-19 12:20:21 -0400

    feat: remove organizations service

--- server/organizations/inputs.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
-
-class OrganizationInput(BaseModel):
-    name: Optional[str]
-    email: Optional[str]
-    website_url: Optional[str]
