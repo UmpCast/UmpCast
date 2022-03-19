f299b3a4d3a719c231cf867f2540c99f49397714
Jonathan Kao <jkao97@yahoo.com>
2022-03-19 12:18:10 -0400

    feat: create models, migrations, admin

--- server/organizations_service/organizations/admin.py
+++ server/organizations_service/organizations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from organizations.models import Organization, Season, Division, Position
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
