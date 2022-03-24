5527634f4fdf91dd5d3452a611dde465e68d8af0
Jonathan Kao <jkao97@yahoo.com>
2022-03-24 13:08:38 -0400

    feat(users_service): update models (#436)
    * feat: squash migrations
    
    * feat: update user model
    
    * feat: add readonly fields to admin
    
    * feat: model fields blank=true

--- server/users_service/users/models.py
+++ server/users_service/users/models.py
@@ -3,8 +3,15 @@ from django.db import models
 
 class User(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    first_name = models.CharField(max_length=255, null=True)
-    last_name = models.CharField(max_length=255, null=True)
-    email = models.EmailField(max_length=255, null=True)
-    address = models.CharField(max_length=255, null=True)
-    phone_number = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    street_address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    zip_code = models.CharField(max_length=5, null=True, blank=True)
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
