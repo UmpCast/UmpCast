93879550a4ede33958a5719134a109cdac3c7371
Victor Lin <vvictor.llin@gmail.com>
2022-03-30 23:14:41 +0000

    test: scaffold season calendar tests

---
+++ client/src/screens/SeasonCalendar/index.spec.tsx
@@ -0,0 +1,12 @@
+import { parameratizableScreenSetup } from '@/testing/setup'
+import SeasonCalendarScreen from '.'
+
+const setup = parameratizableScreenSetup(SeasonCalendarScreen)
+
+it('show games for the current week on render', async () => {})
+
+it('jumps to games for another week', async () => {})
+
+it('moves to games for the past or next week', async () => {})
+
+it('navigates to a game page on click', async () => {})
