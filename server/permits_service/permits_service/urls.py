90bea59c5113840b5175e7817140a24c5bcaedc8
Jonathan Kao <jkao97@yahoo.com>
2022-03-31 16:31:29 -0400

    feat: init graphql server

--- server/permits_service/permits_service/urls.py
+++ server/permits_service/permits_service/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ariadne_django.views import GraphQLView
+from schema.schema import schema
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("graphql/", GraphQLView.as_view(schema=schema), name='graphql'),
 ]
