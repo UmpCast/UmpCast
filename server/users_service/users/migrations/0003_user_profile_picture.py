6a3369cc7251dc6d6e8b73cded7b63d1fd8c7b3f
Jonathan Kao <jkao97@yahoo.com>
2022-03-31 17:35:53 -0400

    fix(ci): users_service, organizations_service workflows (#448)
    * fix: ci
    
    * feat: refactor using black

--- server/users_service/users/migrations/0003_user_profile_picture.py
+++ server/users_service/users/migrations/0003_user_profile_picture.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_alter_user_city_alter_user_phone_number_and_more'),
+        ("users", "0002_alter_user_city_alter_user_phone_number_and_more"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='profile_picture',
-            field=models.ImageField(blank=True, null=True, upload_to='profile_pics/%Y/%m/'),
+            model_name="user",
+            name="profile_picture",
+            field=models.ImageField(
+                blank=True, null=True, upload_to="profile_pics/%Y/%m/"
+            ),
         ),
     ]
