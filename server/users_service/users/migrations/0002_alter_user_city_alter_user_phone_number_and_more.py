fdaf1e0c34d39843ab0e013823eac3965e63880e
Jonathan Kao <jkao97@yahoo.com>
2022-03-31 16:52:03 -0400

    feat: refactor using black

--- server/users_service/users/migrations/0002_alter_user_city_alter_user_phone_number_and_more.py
+++ server/users_service/users/migrations/0002_alter_user_city_alter_user_phone_number_and_more.py
@@ -6,33 +6,33 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='city',
+            model_name="user",
+            name="city",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='phone_number',
+            model_name="user",
+            name="phone_number",
             field=models.CharField(blank=True, max_length=10, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='state',
+            model_name="user",
+            name="state",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='street_address',
+            model_name="user",
+            name="street_address",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='zip_code',
+            model_name="user",
+            name="zip_code",
             field=models.CharField(blank=True, max_length=5, null=True),
         ),
     ]
