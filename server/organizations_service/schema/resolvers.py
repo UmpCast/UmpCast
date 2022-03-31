83306faf4cba9e7e89a182184828d369a91cc441
Jonathan Kao <jkao97@yahoo.com>
2022-03-31 12:26:18 -0400

    feat: implement organization query, organization type

--- server/organizations_service/schema/resolvers.py
+++ server/organizations_service/schema/resolvers.py
@@ -1,8 +1,73 @@
-from ariadne import QueryType
+from datetime import datetime
+from optparse import Option
+from typing import Any, Optional
+
+from ariadne import QueryType, ScalarType
+from ariadne.contrib.federation import FederatedObjectType
+from ariadne.types import GraphQLResolveInfo
+
+from organizations.models import Organization
 
 query = QueryType()
+organization = FederatedObjectType("Organization")
+datetime_scalar = ScalarType("DateTime")
 
 
-@query.field("hello")
-def resolve_hello(_, __) -> str:
-    return "Hello world!"
+@datetime_scalar.serializer
+def serialize_datetime(value: datetime) -> str:
+    return value.isoformat()
+
+
+@query.field("organization")
+def resolve_organization(
+    _: Any, __: GraphQLResolveInfo, id: int
+) -> Optional[Organization]:
+    if Organization.objects.filter(id=id).exists():
+        return Organization.objects.get(id=id)
+    else:
+        return None
+
+
+@organization.reference_resolver("id")
+def resolve_user_reference(
+    _: Any, __: GraphQLResolveInfo, representation: dict[str, Any]
+) -> Organization:
+    return Organization.objects.get(id=representation.get("id"))
+
+
+@organization.field("dateCreated")
+def resolve_date_created(obj: Organization, _: GraphQLResolveInfo) -> datetime:
+    return obj.created_at
+
+
+@organization.field("dateUpdated")
+def resolve_date_updated(obj: Organization, _: GraphQLResolveInfo) -> datetime:
+    return obj.updated_at
+
+
+@organization.field("name")
+def resolve_name(obj: Organization, _: GraphQLResolveInfo) -> str:
+    return obj.name
+
+
+@organization.field("description")
+def resolve_description(obj: Organization, _: GraphQLResolveInfo) -> Optional[str]:
+    return obj.description
+
+
+@organization.field("email")
+def resolve_email(obj: Organization, _: GraphQLResolveInfo) -> Optional[str]:
+    return obj.email
+
+
+@organization.field("logoUrl")
+def resolve_logo_url(obj: Organization, _: GraphQLResolveInfo) -> Optional[str]:
+    if obj.logo:
+        return obj.logo.url
+    else:
+        return None
+
+
+@organization.field("websiteUrl")
+def resolve_website_url(obj: Organization, _: GraphQLResolveInfo) -> Optional[str]:
+    return obj.website_url
--- server/organizations_service/schema/schema.py
+++ server/organizations_service/schema/schema.py
@@ -1,7 +1,7 @@
 from ariadne import load_schema_from_path
 from ariadne.contrib.federation import make_federated_schema
 
-from schema.resolvers import query
+from schema.resolvers import query, organization, datetime_scalar
 
 type_defs = load_schema_from_path("schema/schema.graphql")
-schema = make_federated_schema(type_defs, [query])
+schema = make_federated_schema(type_defs, [query, organization, datetime_scalar])
