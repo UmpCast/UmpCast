f1dab9cebcd60d336e3d7e6dec7c16841da1e536
Jonathan Kao <jkao97@yahoo.com>
2022-03-31 16:36:56 -0400

    feat(organizations_service): implement schema (#447)
    * feat: add pillow deps
    
    * feat: add description, logos fields
    
    * feat: implement organization query, organization type
    
    * feat: add pydantic deps
    
    * feat: implement create organizations
    
    * feat: implement update organizations

---
+++ server/organizations_service/schema/inputs.py
@@ -0,0 +1,35 @@
+import base64
+from typing import Optional
+from uuid import uuid4
+
+from django.core.files.base import ContentFile
+from pydantic import BaseModel
+
+
+class CreateOrganizationInput(BaseModel):
+    name: str
+
+
+class UpdateOrganizationInput(BaseModel):
+    organization_id: int
+    name: Optional[str]
+    description: Optional[str]
+    email: Optional[str]
+    logo_b_64: Optional[str]
+    website_url: Optional[str]
+
+    @property
+    def logo(self) -> Optional[ContentFile]:
+        if self.logo_b_64 is not None:
+            format, img_str = self.logo_b_64.split(";base64,")
+            ext = format.split("/")[-1]
+            return ContentFile(base64.b64decode(img_str), name=uuid4().hex + "." + ext)
+        return None
+
+    def dict(self) -> dict:
+        values = super().dict(
+            exclude_unset=True, exclude={"organization_id", "logo_b_64"}
+        )
+        if self.logo is not None:
+            values["logo"] = self.logo
+        return values
