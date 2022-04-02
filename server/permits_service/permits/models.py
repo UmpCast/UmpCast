3d49f360f651200c4bf28b4b87e5978c0a0848ff
Toby Hirsch <thirsch@seas.upenn.edu>
2022-04-01 23:33:18 -0400

    added permits models

--- server/permits_service/permits/models.py
+++ server/permits_service/permits/models.py
@@ -10,3 +10,30 @@ class UserOrganization(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserSeason(models.Model):
+    user_id = models.CharField(max_length=255)
+    season_id = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    max_casts = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserRole(models.Model):
+    user_id = models.CharField(max_length=255)
+    role_id = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserGameRole(models.Model):
+    user_id = models.CharField(max_length=255)
+    game_id = models.CharField(max_length=255)
+    role_id = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
