c11fdf571a1cbbfce4c501e2b6611034166cd0bb
Toby Hirsch <thirsch@seas.upenn.edu>
2022-04-01 23:48:06 -0400

    fixed admin page

--- server/organizations_service/organizations/admin.py
+++ server/organizations_service/organizations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from organizations.models import Division, Organization, Position, Season
+from organizations.models import Division, Organization, Position, Season, Game
 
 
 @admin.register(Organization)
@@ -20,4 +20,9 @@ class DivisionAdmin(admin.ModelAdmin):
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", "updated_at")
