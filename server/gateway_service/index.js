d4c4108bf1bb4ea64d9f01310462ee912950cd0f
Jonathan Kao <jkao97@yahoo.com>
2022-04-05 01:08:39 -0400

    gateway minimal example

--- server/gateway_service/index.js
+++ server/gateway_service/index.js
@@ -1,48 +1,19 @@
-import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway'
+import { ApolloGateway } from '@apollo/gateway'
 import { ApolloServer } from 'apollo-server'
-import { verifyIdToken } from './firebase_auth.js'
-
-class AuthenticatedDataSource extends RemoteGraphQLDataSource {
-    willSendRequest({ request, context }) {
-        request.http.headers.set('user-id', context.id)
-        request.http.headers.set('user-email', context.email)
-        request.http.headers.set('user-email-verified', context.emailVerified)
-    }
-}
 
 const gateway = new ApolloGateway({
     serviceList: [
-        { name: 'users', url: 'http://users-service:8000/graphql' },
+        { name: 'users', url: 'http://users-service:8000/graphql/' },
         {
             name: 'organizations',
-            url: 'http://organizations-service:8000/graphql',
+            url: 'http://organizations-service:8000/graphql/',
         },
-        { name: 'permits', url: 'http://permits-service:8000/graphql' },
+        { name: 'permits', url: 'http://permits-service:8000/graphql/' },
     ],
-    buildService({ name, url }) {
-        return new AuthenticatedDataSource({ url })
-    },
 })
 
 const server = new ApolloServer({
     gateway,
-    context: async ({ req }) => {
-        try {
-            const idToken = req.headers.authorization
-            const user = await verifyIdToken(idToken)
-            return {
-                id: user.user_id,
-                email: user.email,
-                emailVerified: user.email_verified,
-            }
-        } catch (error) {
-            return {
-                id: '',
-                email: '',
-                emailVerified: false,
-            }
-        }
-    },
 })
 
 server
