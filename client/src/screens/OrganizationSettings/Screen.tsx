7278831236b227c4086c2b0c9eb3a82ff22a113d
Victor Lin <vvictor.llin@gmail.com>
2022-04-10 01:51:07 +0000

    feat: replace org settings profile with settings components

--- client/src/screens/OrganizationSettings/Screen.tsx
+++ client/src/screens/OrganizationSettings/Screen.tsx
@@ -2,11 +2,13 @@ import { AntDesign } from '@expo/vector-icons'
 import { Box, VStack } from 'native-base'
 
 import OrgDeleteButton from '@/features/Org/core/Delete/Button'
-import OrgSettingsItem from '@/features/Org/core/Settings/Item'
 import OrgSettingsItemIcon from '@/features/Org/core/Settings/ItemIcon'
 import { useOrgSettingsScreenQuery } from '@/generated'
 import { RootStackRoute } from '@/navigation/navigators/Root/Stack'
 import { RootStackScreenProps } from '@/navigation/screenProps'
+import SettingsItem from '@/components/Settings/Item'
+import SettingsItemPressable from '@/components/Settings/ItemPressable'
+import SettingsItemGroup from '@/components/Settings/ItemGroup'
 
 type ScreenProps = RootStackScreenProps<RootStackRoute.OrganizationSettings>
 
@@ -27,15 +29,28 @@ export default function OrgSettingsScreen({ navigation, route }: ScreenProps) {
     return (
         <Box p={4}>
             <VStack space={4}>
-                <OrgSettingsItem
-                    icon={<OrgSettingsItemIcon as={AntDesign} name="edit" />}
-                    onPress={() => {
-                        navigate(RootStackRoute.OrganizationSettingsProfile, {
-                            orgId
-                        })
-                    }}
-                    title="Edit Profile"
-                />
+                <SettingsItemGroup>
+                    <SettingsItemPressable
+                        onPress={() => {
+                            navigate(
+                                RootStackRoute.OrganizationSettingsProfile,
+                                {
+                                    orgId
+                                }
+                            )
+                        }}
+                    >
+                        <SettingsItem
+                            icon={
+                                <OrgSettingsItemIcon
+                                    as={AntDesign}
+                                    name="edit"
+                                />
+                            }
+                            title="View profile"
+                        />
+                    </SettingsItemPressable>
+                </SettingsItemGroup>
                 <OrgDeleteButton onDelete={goBack} org={data.organization} />
             </VStack>
         </Box>
