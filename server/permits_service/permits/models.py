c4480289ac11ffeca1919a32f8196e350c0b3460
Jonathan Kao <jkao97@yahoo.com>
2022-04-17 22:12:53 -0400

    feat: remove is_member field

--- server/permits_service/permits/models.py
+++ server/permits_service/permits/models.py
@@ -6,7 +6,6 @@ class UserOrganization(models.Model):
     organization_id = models.CharField(max_length=255)
 
     is_owner = models.BooleanField(default=False)
-    is_member = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
