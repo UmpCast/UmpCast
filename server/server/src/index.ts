f2a719bc585b4b71d8a3a06ff4c1e3053498ad55
Jonathan Kao <jkao97@yahoo.com>
2022-05-09 01:33:52 -0700

    auto-find resolvers

--- server/server/src/index.ts
+++ server/server/src/index.ts
@@ -1,19 +1,18 @@
 import "reflect-metadata";
 import { createContext } from "./context";
 import { ApolloServer } from "apollo-server";
-import { PingResolver } from "./resolver";
 import { buildSchema } from "type-graphql";
 
 async function main() {
-  const schema = await buildSchema({
-    resolvers: [PingResolver],
-  });
-  const server = new ApolloServer({
-    schema: schema,
-    context: createContext,
-  });
-  const { url } = await server.listen(4000);
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
+    const schema = await buildSchema({
+        resolvers: [__dirname + "/resolvers/**/*.ts"],
+    });
+    const server = new ApolloServer({
+        schema: schema,
+        context: createContext,
+    });
+    const { url } = await server.listen(4000);
+    console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
 
 main();
