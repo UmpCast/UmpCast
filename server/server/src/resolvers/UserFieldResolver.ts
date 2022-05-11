ec0c5a645bd24f18b859be93f2a9ab8608ab3e7a
Jonathan Kao <jkao97@yahoo.com>
2022-05-11 12:54:45 -0700

    add user field resolver

---
+++ server/server/src/resolvers/UserFieldResolver.ts
@@ -0,0 +1,23 @@
+import { Arg, Ctx, FieldResolver, ID, Resolver, Root } from "type-graphql";
+import { GraphQLContext } from "../context";
+import { User } from "../types/User";
+import { UserParticipatingSeasonEdge } from "../types/UserParticipatingSeasonEdge";
+
+@Resolver(() => User)
+export class UserFieldResolver {
+    @FieldResolver(() => UserParticipatingSeasonEdge, { nullable: true })
+    async season(
+        @Root() user: User,
+        @Ctx() { prisma }: GraphQLContext,
+        @Arg("seasonId", () => ID) seasonId: string,
+    ): Promise<UserParticipatingSeasonEdge | null> {
+        return prisma.userSeason.findUnique({
+            where: {
+                userId_seasonId: {
+                    userId: user.id,
+                    seasonId: Number(seasonId),
+                },
+            },
+        });
+    }
+}
