aae17805322865a699630042ce13ad3a1282fde3
Jonathan Kao <jkao97@yahoo.com>
2022-05-11 15:13:14 -0700

    add GameListing type, resolvers

---
+++ server/server/src/resolvers/GameListingCrudResolver.ts
@@ -0,0 +1,18 @@
+import { Arg, Ctx, ID, Query, Resolver } from "type-graphql";
+import { GraphQLContext } from "../context";
+import { GameListing } from "../types/GameListing";
+
+@Resolver(() => GameListing)
+export class GameListingCrudResolver {
+    @Query(() => GameListing, { nullable: true })
+    async gameListing(
+        @Ctx() { prisma }: GraphQLContext,
+        @Arg("id", () => ID) id: string,
+    ): Promise<GameListing | null> {
+        return prisma.listing.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+    }
+}
