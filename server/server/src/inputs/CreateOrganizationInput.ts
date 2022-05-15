0614337dd43031e56fb322caa7698f97f8c388dd
Jonathan Kao <jkao97@yahoo.com>
2022-05-14 19:22:33 -0700

    add @IsOptional validation to optional fields

--- server/server/src/inputs/CreateOrganizationInput.ts
+++ server/server/src/inputs/CreateOrganizationInput.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsUrl } from "class-validator";
+import { IsEmail, IsOptional, IsUrl } from "class-validator";
 import { Field, InputType } from "type-graphql";
 
 @InputType()
@@ -8,12 +8,15 @@ export class CreateOrganizationInput {
 
     @Field(() => String, { nullable: true })
     @IsEmail()
+    @IsOptional()
     email!: string | null;
 
     @Field(() => String, { nullable: true })
     @IsUrl()
+    @IsOptional()
     websiteUrl!: string | null;
 
     @Field(() => String, { nullable: true })
+    @IsOptional()
     description!: string | null;
 }
