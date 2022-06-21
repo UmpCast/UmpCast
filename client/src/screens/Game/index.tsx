cdee411ba1c0cc6ffd4cb338425475249b7eed37
Victor Lin <vvictor.llin@gmail.com>
2022-06-21 14:58:55 -0700

    feat: add time and location to game screen

--- client/src/screens/Game/index.tsx
+++ client/src/screens/Game/index.tsx
@@ -1,10 +1,12 @@
-import { Heading, HStack, Text, VStack } from 'native-base'
+import { Heading, HStack, Icon, Text, VStack } from 'native-base'
 
 import ScreenContainer from '@/components/Screen/Container'
 import OrgProfileLogo from '@/features/Org/core/Profile/Logo'
 import { useGameScreenQuery } from '@/generated'
 import { RootStackRoute } from '@/navigation/navigators/Root/Stack'
 import { RootStackScreenProps } from '@/navigation/types'
+import { Feather } from '@expo/vector-icons'
+import { UserAvatar } from '@/features/User/components/Avatar'
 
 type GameScreenProps = RootStackScreenProps<RootStackRoute.Game>
 
@@ -28,18 +30,30 @@ export default function GameScreen({ route }: GameScreenProps) {
 
     return (
         <ScreenContainer>
-            <VStack space={2}>
-                <HStack alignItems="center" space={3}>
-                    <OrgProfileLogo org={organization} size={20} />
-                    <Text
-                        color="secondary.500"
-                        fontSize="md"
-                        fontWeight="semibold"
-                    >
-                        {season.name} / {division.name}
-                    </Text>
+            <VStack space={4}>
+                <VStack>
+                    <HStack alignItems="center" space={3}>
+                        <OrgProfileLogo org={organization} size={20} />
+                        <Text
+                            color="secondary.500"
+                            fontSize="md"
+                            fontWeight="semibold"
+                        >
+                            {season.name} / {division.name}
+                        </Text>
+                    </HStack>
+                    <Heading>{game.name}</Heading>
+                </VStack>
+                <HStack space="3">
+                    <Icon as={Feather} name="clock" />
+                    <Text>Fri, Mar 3 12pm - 1:30pm</Text>
                 </HStack>
-                <Heading>{game.name}</Heading>
+                <HStack space="3">
+                    <Icon as={Feather} name="map-pin" />
+                    <Text>Mitchell Field Ball Park</Text>
+                </HStack>
+                <Text bold>Assignees</Text>
+                <UserAvatar></UserAvatar>
             </VStack>
         </ScreenContainer>
     )
