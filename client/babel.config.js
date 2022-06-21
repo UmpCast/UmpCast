e46c17ced7232429ee0167b6eb3af0deef60b7fb
victor.lin <victor.lin@bytedance.com>
2022-06-21 13:19:55 -0700

    chore: can start client

--- client/babel.config.js
+++ client/babel.config.js
@@ -3,6 +3,12 @@ module.exports = {
     plugins: [
         ['@babel/plugin-proposal-export-default-from'],
         [
+            'import-graphql',
+            {
+                runtime: true
+            }
+        ],
+        [
             'module-resolver',
             {
                 alias: {
--- client/src/server/schema.ts
+++ client/src/server/schema.ts
@@ -1,23 +1,24 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-import divisionSchema from './schema/division'
-import firebaseSchema from './schema/firebase'
-import gameSchema from './schema/game'
-import organizationSchema from './schema/organization'
-import positionSchema from './schema/position'
-import rootSchema from './schema/root'
-import seasonSchema from './schema/season'
-import userSchema from './schema/user'
+// graphql plugin import errors on absolute paths
+import division from './schema/division.graphql'
+import firebase from './schema/firebase.graphql'
+import game from './schema/game.graphql'
+import organization from './schema/organization.graphql'
+import position from './schema/position.graphql'
+import root from './schema/root.graphql'
+import season from './schema/season.graphql'
+import user from './schema/user.graphql'
 
 export const mockSchema = makeExecutableSchema({
     typeDefs: [
-        divisionSchema,
-        firebaseSchema,
-        gameSchema,
-        organizationSchema,
-        positionSchema,
-        rootSchema,
-        seasonSchema,
-        userSchema
+        division,
+        firebase,
+        game,
+        organization,
+        position,
+        root,
+        season,
+        user
     ]
 })
